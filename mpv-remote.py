57621854119566b84afef95664d05ea775f3e336
Pavel Zolotov <yalososka@gmail.com>
2021-07-20 23:24:05 +0300

    start mpv and django server

--- mpv-remote.py
+++ mpv-remote.py
@@ -1,2 +1,17 @@
-print('hello')
-input()
+import sys,os
+import datetime
+
+def kill_processes(proc):
+    stream = os.popen('pgrep '+proc)
+    ids = stream.read().split()
+    for id in ids:
+        os.popen('kill -9 '+id)
+
+kill_processes('-f "manage.py runserver"')
+kill_processes('mpv')
+
+os.popen('python3 '+os.path.dirname(os.path.realpath(__file__))+'/manage.py runserver 0.0.0.0:8000')
+
+if len(sys.argv)>1:
+    stream = os.popen('mpv '+'--input-ipc-server=/tmp/mpvsocket '+' '.join(sys.argv[1:]))
+    stream.read()
