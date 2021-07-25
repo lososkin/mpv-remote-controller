cffff9b49227f92bc89185923f7a61b79ec7f556
Pavel Zolotov <yalososka@gmail.com>
2021-07-25 03:55:28 +0300

    feat: add subtitles from file

--- remote/urls.py
+++ remote/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('command/', views.command),
+    path('getdirs/', views.getDirs),
+    path('changedir/', views.changeDir),
 ]
--- remote/views.py
+++ remote/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-import json, socket
+import json, socket, os
 
 def index(request):
     return render(request, 'remote/index.html')
@@ -42,3 +42,28 @@ def command(request):
     s.send(bytes(command, 'utf-8') + b'\n')
     s.close()
     return HttpResponse("OK", status = 200)
+
+@csrf_exempt
+def getDirs(request):
+    res = {'dirs':[],'files':[]}
+    for x in os.listdir():
+        if os.path.isdir(x):
+            res['dirs'].append(x)
+        else:
+            res['files'].append(x)
+    res['dirs'].sort()
+    res['files'].sort()
+    return HttpResponse(json.dumps(res) ,status=200)
+
+@csrf_exempt
+def changeDir(request):
+    d = json.loads(request.body)['dir']
+    if os.path.isdir(os.path.join(os.getcwd(),d)):
+        os.chdir(d)
+        return HttpResponse(json.dumps({'type':'dir'}), status = 200)
+    else:
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.connect("/tmp/mpvsocket")
+        s.send(bytes('{ "command": ["sub-add", "'+os.path.join(os.getcwd(),d)+'"]}', 'utf-8') + b'\n')
+        s.close()
+        return HttpResponse(json.dumps({'type':'file'}), status = 200)
