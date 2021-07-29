e1050ee9f601bcd5b5c00e75c32955e10f97a8ea
Pavel Zolotov <yalososka@gmail.com>
2021-07-29 14:21:38 +0300

    Separate html and css

--- mpvremote/urls.py
+++ mpvremote/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('remote.urls')),
-]
+] + static(settings.STATIC_URL)
